71fdd11cdc17e21f79cf78198e2f7d611f90ea73
vizzamaed <vizzamaedollente@gmail.com>
2023-10-04 23:55:34 +0800

    PHP Syntax Overview

--- dollente.php
+++ dollente.php
@@ -6,6 +6,23 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+    //PHP Syntax Overview
+    $age = 2023 - 2000;
+    $greetings= "Hello!, ";
+    $name= "Viz";
+    print("$greetings $name<br>");
+    print("You are $age years old.<br>");
+
+    if ($age > 17)
+    {
+    print("You passed the minimum age requirement!");
+    }
+    else
+    {
+    print("Sorry, your age is not allowed");
+    }
+    ?>
 </body>
+
 </html>
